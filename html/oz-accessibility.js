3fe1b002ea0f99c74f6de0bb82a0b45774099367
ooz <ooz@users.noreply.github.com>
2021-06-10 22:26:47 +0200

    Update oz style for smaller default font, relative units and font toggle controls

--- html/oz-accessibility.js
+++ html/oz-accessibility.js
@@ -1,3 +1,4 @@
 function toggleTheme() { document.body.classList.toggle("dark-mode") }
 function initTheme() { let h=new Date().getHours(); if (h <= 8 || h >= 20) { toggleTheme() } }
+function toggleFontSize() { document.body.classList.toggle("large-font") }
 
